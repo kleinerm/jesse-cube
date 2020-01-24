081d69447b644a3aaaa9ffd39c3ccf6c80a582f9
kleinerm <mario.kleiner.de@gmail.com>
2020-01-24 05:10:44 +0100

    glew.c: Hack to find wglew.h in main folder.
    No need for  a GL subfolder or system GL folder on Windows.

--- glew.c
+++ glew.c
@@ -50,7 +50,7 @@
 #  if defined(NOGDI)
 #    undef NOGDI
 #  endif
-#  include <GL/wglew.h>
+#  include "wglew.h"
 #elif !defined(__ANDROID__) && !defined(__native_client__) && !defined(__HAIKU__) && (!defined(__APPLE__) || defined(GLEW_APPLE_GLX))
 #  include <GL/glxew.h>
 #endif
